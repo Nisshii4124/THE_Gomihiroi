64d1e67bc472bc7163d72dec4ea7fd92cc489314
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2017-02-28 12:15:24 +0900

    no message

--- Assets/Script/gomiget.cs
+++ Assets/Script/gomiget.cs
@@ -10,12 +10,7 @@ public class gomiget : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-<<<<<<< HEAD
-
-=======
-        Hantei = GetComponent<Collider>();
         Hantei.enabled = false;
->>>>>>> 332b66383cef3293f8ffe2b717d2e5b327dfee87
     }
     // Update is called once per frame
     void Update()
@@ -37,51 +32,12 @@ public class gomiget : MonoBehaviour
 
             if (other.gameObject.tag == "gomi")//tag「gomi」のついたオブジェクトにふれたら
             {
-<<<<<<< HEAD
                 if (Input.GetKey(KeyCode.UpArrow))
                 {
-<<<<<<< HEAD
-//<<<<<<< HEAD:Assets/Script/gomiget_2.cs
-=======
-
->>>>>>> 332b66383cef3293f8ffe2b717d2e5b327dfee87
-
                     TimeandScore.gomi += 1;
-                    Destroy(gameObject);//オブジェクトを消す
+                    Destroy(other.gameObject);//オブジェクトを消す
 
-<<<<<<< HEAD
-//=======
-                    TimeandScore.gomi += 1;
-                    Destroy(gameObject);//オブジェクトを消す
-//<<<<<<< HEAD
-//=======
-//>>>>>>> 01a5b2366c78b0157f6c1cff37d9525b4aa3364c:Assets/Script/gomiget.cs
-=======
-
-                    TimeandScore.gomi += 1;
-                    Destroy(gameObject);//オブジェクトを消す
-                    gomiflg = true;
-
->>>>>>> 332b66383cef3293f8ffe2b717d2e5b327dfee87
-
-                    TimeandScore.gomi += 1;
-                    Destroy(gameObject);//オブジェクトを消す
-
-
-<<<<<<< HEAD
-//<<<<<<< HEAD:Assets/Script/gomiget_2.cs
-
-//=======
-//>>>>>>> 01a5b2366c78b0157f6c1cff37d9525b4aa3364c:Assets/Script/gomiget.cs
-//>>>>>>> 6cf62f48ddaf38cf792921a52dd5d9c806c865dc
-=======
-
->>>>>>> 332b66383cef3293f8ffe2b717d2e5b327dfee87
                 }
-=======
-                TimeandScore.gomi += 1;
-                Destroy(other.gameObject);//オブジェクトを消す
->>>>>>> 9595a84657f0ca04a44f09c2187fbd337f429822
             }
         }
     }
