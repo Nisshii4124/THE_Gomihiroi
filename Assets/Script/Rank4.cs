9e982907ca081e9aaaf0c58b079573700fba092b
nakashima <nkc.kazuki.nakashima@gmail.com>
2017-03-01 13:50:49 +0900

    ステージすべてちゃんと動けます ステージ一つにつきランキング一つ実装できました

--- Assets/Script/Rank4.cs
+++ Assets/Script/Rank4.cs
@@ -35,7 +35,7 @@ public class Rank4 : MonoBehaviour
         Ranking.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 120);
 
         // 表示するテキスト
-        Ranking.GetComponent<Text>().text = "TOP 5";
+        Ranking.GetComponent<Text>().text = "   TOP 5";
 
         // ランキング表示
         for (int i = 0; i < RANKING_NUM4; i++)
