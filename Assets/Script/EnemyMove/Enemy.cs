f2aa2cf9ed3dead67e792c12b3670bb19d993f61
Nisshii <nkc.daiki.nishiwaki@gmail.com>
2017-03-02 14:55:39 +0900

    ステージ２復旧　カラス修正

--- Assets/Script/EnemyMove/Enemy.cs
+++ Assets/Script/EnemyMove/Enemy.cs
@@ -8,15 +8,45 @@ public class Enemy : MonoBehaviour
     public GameObject target;
     NavMeshAgent agent;
 
-	// Use this for initialization
-	void Start ()
+    bool tuibi = true;
+    float a = 0;
+
+    // Use this for initialization
+    void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-        agent.destination = target.transform.position;
-	}
+        if (tuibi == true)
+            agent.destination = target.transform.position;
+        else if (tuibi == false)
+            agent.destination = -target.transform.position;
+
+        if (tuibi == false)
+        {
+            a = a + Time.deltaTime;
+            if (a >= 4)
+            {
+                tuibi = true;
+                a = 0;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.tag == "Player")
+        //    tuibi = false;
+        if (other.gameObject.tag == "field")
+        {
+            tuibi = false;
+            if (TimeandScore.gomi > 0)
+            {
+                TimeandScore.gomi--;
+            }
+        }
+    }
 }
