1b24dfc1c1c8d064aefa3cb11dbacabc056cb832
Nisshii <nkc.daiki.nishiwaki@gmail.com>
2017-03-08 13:45:06 +0900

    ステージセレクトの画像の変更

--- Assets/Script/Stagechange.cs
+++ Assets/Script/Stagechange.cs
@@ -26,52 +26,26 @@ public class Stagechange : MonoBehaviour
 
     int a = 2, b = 4;
 
-    bool Stage1flg = false;
-    bool Stage2flg = false;
-    bool Stage3flg = false;
-    bool Stage4flg = false;
-    bool Stage5flg = false;
-
-    public Image StageFadeImage;          // フェードに利用する画像保存用変数
-
-    bool isFade;        // フェードスイッチ
-    bool isFadeIn;      // フェードインフラグ
-    bool isFadeOut;     // フェードアウトフラグ
     float alpha;        // アルファ値計算用変数
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         //xxxImage = GameObject.Find("XxxImage").GetComponent<Image>();
-
-        // シーン開始時はフェードインから始まる
-        isFade = true;  // フェードON
-        isFadeIn = true;  // フェードインON
-        isFadeOut = false; // フェードアウトOFF
         alpha = 1.0f;  // 100％不透明
-
-        // 最前面へ
-        //this.transform.SetAsLastSibling();
-
-        // 画像のアルファ値をセット
-        StageFadeImage.color = new Color(StageFadeImage.color.r, StageFadeImage.color.g, StageFadeImage.color.b, alpha);
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         if (StageSelect.SelectStage == 1)
         {
             xxxImage.sprite = Stage1;
             time1 += Time.deltaTime;
             if (time1 >= a)
-                FadeOut();
+                xxxImage.sprite = Stage1_1;
             if (time1 >= b)
-            {
-                xxxImage.sprite = Stage1;
-
                 time1 = 0;
-            }
         }
         else
             time1 = 0;
@@ -122,66 +96,6 @@ public class Stagechange : MonoBehaviour
         if (StageSelect.SelectStage == 6)
         {
             xxxImage.sprite = Stage6;
-        }
-
-
-
-
-        if (isFade == true)
-        {
-            if (isFadeIn == true)
-            {
-                FadeIn();    // フェードイン
-            }
-            if (isFadeOut == true)
-            {
-                FadeOut();    // フェードアウト
-            }
-        }
-        else
-        {
-            // Eneterキーでフェードアウト開始し、終了後シーン遷移
-            if (Input.GetKey(KeyCode.Space))
-            {
-                isFade = true;                   // フェードフラグON
-                isFadeOut = true;                   // フェードアウトフラグON
-                //this.transform.SetAsLastSibling();  // 最前面へ
-            }
-        }
-    }
-    void FadeIn()
-    {
-        // 計算用アルファ値を減算
-        alpha -= 0.04f;
-
-        // 画像のアルファ値を変更
-        StageFadeImage.color = new Color(StageFadeImage.color.r, StageFadeImage.color.g, StageFadeImage.color.b, alpha);
-
-        // アルファ値がMIN値以下になったら
-        if (StageFadeImage.color.a <= 0.0f)
-        {
-            isFade = false;                      // フェードスイッチOFF
-            isFadeIn = false;                      // フェードインフラグOFF
-            //this.transform.SetAsFirstSibling();    // 最背面へ
-        }
-    }
-
-    // フェードアウト（不透明にしていく）
-    void FadeOut()
-    {
-        alpha += 0.04f; // 計算用アルファ値を加算
-
-        // 画像のアルファ値を変更
-        StageFadeImage.color = new Color(StageFadeImage.color.r, StageFadeImage.color.g, StageFadeImage.color.b, alpha);
-
-        // アルファ値がMAX値以上になったら
-        if (StageFadeImage.color.a >= 1.0f)
-        {
-            xxxImage.sprite = Stage1_1;
-
-            isFade = true;
-            isFadeOut = false;                        // フェードアウトフラグOFF
-            isFadeIn = true;                        // フェードスイッチON
         }
     }
 }
